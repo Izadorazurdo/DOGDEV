1d8c7d7671706cc09d07ca73a27692171d131703
Izadorazurdo <izadora.zurdo.1bmangal@gmail.com>
2025-02-19 18:54:42 +0000

    .

---
+++ dogdev/assets/js/script.js
@@ -0,0 +1,21 @@
+//menu
+let menuOpener = document.querySelector('.menu-opener');
+//nav
+let nav = document.querySelector('header nav');
+//ouvinte para o eventoclick no menuopener
+menuOpener.addEventListener('click',()=>{
+    if(nav.classList.contains('opened')){
+        nav.classList.remove('opened');
+        menuOpener.querySelector('.close-icon')
+        .style .display='none';
+        menuOpener.querySelector('.hamburger-icon')
+        .style .display='flex';
+
+    }else{
+        nav.classList.add('opened');
+        menuOpener.querySelector('.close-icon')
+        .style .display='flex';
+        menuOpener.querySelector('.hamburger-icon')
+        .style .display='none';
+    }
+});
