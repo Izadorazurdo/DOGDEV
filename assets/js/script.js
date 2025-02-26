4b475cb172c107b5edb7891eaadb8c44122705e3
Izadorazurdo <izadora.zurdo.1bmangal@gmail.com>
2025-02-26 15:24:54 +0000

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,11 @@ menuOpener.addEventListener('click',()=>{
         .style .display='none';
     }
 });
+//TESTEMONIALS
+//vetor de objetos contendo as frases e os icones
+let testemonials = [
+    {quote:'"Mais do que nunca, os animais' + 'são tratados como membros da familia"',origin:'cbs.svg' }, 
+    {quote:'"DogDev é um serviço de entrega direto ao'+'consumidor, preparado na hora com ingredientes 100%'+'reais.Ingredientes que humanos reconheceriam"' ,origin:'forbes.svg' }, 
+    {quote:'"DogDev usa ingredientes simples e limpos em '+ 'seus produtos"' ,origin:'fox.svg' }, 
+    {quote:'"Vejo você no Dogdev como um verdadeiro héroi"' ,origin:'sharktank.svg' }
+];
